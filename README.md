ea3002183ff1cb2217d9985aacd910a4193023b6
jeremystretch <jstretch@ns1.com>
2022-08-17 13:09:30 -0400

    Add protection against changing the admin user's active status

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This plugin serves to ease the administration of the public NetBox demo instance at https://demo.netbox.dev. It performs the following tasks:
 
-* Prevent modification or deletion of the default admin user
+* Prevent modification of the admin user name, password, or active status
+* Prevent deletion of the admin user
+
--- netbox_demo/signals.py
+++ netbox_demo/signals.py
@@ -7,13 +7,15 @@ from django.dispatch import receiver
 @receiver(pre_save, sender=User)
 def prevent_admin_user_change(instance, raw=False, **kwargs):
     """
-    Prevent changes to the admin user's username or password.
+    Prevent changes to the admin user's username, password, or active status.
     """
     if instance.pk == 1:
         if instance.username != settings.PLUGINS_CONFIG['netbox_demo']['admin_username']:
             raise Exception("Admin user may not be renamed.")
         if instance._password:
             raise Exception("Admin user password may not be changed.")
+        if not instance.is_active:
+            raise Exception("Admin user may not be disabled.")
 
 
 @receiver(pre_delete, sender=User)
