86ac4f376e4c35fe338d16ce8af831ce42f05966
Jeremy Stretch <jstretch@netboxlabs.com>
2024-05-06 15:59:41 -0400

    Update plugin version

--- netbox_demo/__init__.py
+++ netbox_demo/__init__.py
@@ -5,7 +5,7 @@ class NetBoxDemoConfig(PluginConfig):
     name = 'netbox_demo'
     verbose_name = 'Demo'
     description = 'Demo maintenance plugin'
-    version = '0.3.1'
+    version = '0.5.0'
     base_url = 'demo'
     default_settings = {
         'admin_username': 'admin',
